d26663ed162e3fc9f3611264cc602f673afa5ba4
Jin Kim <jinyoungkim927@gmail.com>
2025-12-06 16:10:45 -0500

    Fix homepage to show all posts using useStaticQuery

--- src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -36,4 +36,3 @@ const Footer = () => {
 }
 
 export default Footer
-
--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -1,30 +1,34 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { HeadFC } from "gatsby"
+import { HeadFC, graphql, useStaticQuery } from "gatsby"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 import Listing from "@lekoarts/gatsby-theme-minimal-blog/src/components/listing"
 import Seo from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo"
 
-export type MBHomepageProps = {
-  posts: {
-    slug: string
-    title: string
-    date: string
-    excerpt: string
-    description: string
-    timeToRead?: number
-    tags?: {
-      name: string
-      slug: string
-    }[]
-  }[]
-}
+// Homepage fetches ALL posts (not limited to 3)
+const Homepage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allPost(sort: { date: DESC }) {
+        nodes {
+          slug
+          title
+          date(formatString: "MMMM D, YYYY")
+          excerpt
+          timeToRead
+          description
+          tags {
+            name
+            slug
+          }
+        }
+      }
+    }
+  `)
 
-// Homepage is now identical to blog - just shows all posts
-const Homepage = ({ posts }: MBHomepageProps) => {
   return (
     <Layout>
-      <Listing posts={posts} sx={{ mt: [4, 5] }} />
+      <Listing posts={data.allPost.nodes} sx={{ mt: [4, 5] }} />
     </Layout>
   )
 }
