d6e6d5e54885757bf1e2d7c0ae736ceff7786145
Jin Kim <jinyoungkim927@gmail.com>
2025-12-06 12:35:32 -0500

    Fix runtime error: remove DOM manipulation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,1 @@
 import "./src/styles/custom.css"
-
-// Add click handler for footer disclaimer link
-export const onClientEntry = () => {
-  if (typeof window !== "undefined") {
-    const observer = new MutationObserver(() => {
-      const footer = document.querySelector("footer")
-      if (footer && !footer.dataset.customized) {
-        footer.dataset.customized = "true"
-        footer.addEventListener("click", () => {
-          window.location.href = "/disclaimer/"
-        })
-      }
-    })
-    observer.observe(document.body, { childList: true, subtree: true })
-  }
-}
-
