d5577b4ab49dcca60935f4be807d58033684a88f
Jin Kim <jinyoungkim927@gmail.com>
2025-12-06 11:06:25 -0500

    Add disclaimer page with licensing info

--- src/@lekoarts/gatsby-theme-minimal-blog/src/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/src/components/footer.tsx
@@ -1,4 +1,24 @@
-// Empty footer - hyper minimalist
-const Footer = () => null
+/** @jsx jsx */
+import { jsx, Box } from "theme-ui"
+import { Link } from "gatsby"
+
+const Footer = () => (
+  <Box
+    as="footer"
+    sx={{
+      mt: 5,
+      pt: 4,
+      borderTop: "1px solid",
+      borderColor: "divide",
+      color: "secondary",
+      fontSize: 0,
+      textAlign: "center",
+    }}
+  >
+    <Link to="/disclaimer" sx={{ color: "secondary", textDecoration: "none", "&:hover": { color: "primary" } }}>
+      Disclaimer
+    </Link>
+  </Box>
+)
 
 export default Footer
