ef3e11d9fb99a727efdfbc893c0f42cd9e71473e
Jin Kim <jinyoungkim927@gmail.com>
2025-12-05 22:58:25 -0500

    Hide footer completely, add root wrapper to prevent dark mode toggle

--- src/@lekoarts/gatsby-theme-minimal-blog/src/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/src/components/footer.tsx
@@ -1,24 +1,7 @@
 /** @jsx jsx */
 
 const Footer = () => {
-  return (
-    <footer
-      sx={{
-        boxSizing: `border-box`,
-        display: `flex`,
-        justifyContent: `space-between`,
-        mt: [6],
-        color: `secondary`,
-        a: {
-          variant: `links.secondary`,
-        },
-        flexDirection: [`column`, `column`, `row`],
-        variant: `dividers.top`,
-      }}
-    >
-      {/* Footer content removed */}
-    </footer>
-  )
+  return null
 }
 
 export default Footer
