9e4818b9a54e61b1d06e5429f5d6f51c37768de1
Jin Kim <jinyoungkim927@gmail.com>
2026-01-04 17:02:56 -0500

    Fix header to use shadowed HeaderTitle with animations
    The header was importing HeaderTitle from the original theme package
    instead of the local shadowed component, causing the AnimatedUseless
    component to not render in production.

--- src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
@@ -2,7 +2,7 @@
 import { jsx, Flex } from "theme-ui"
 import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
 import Navigation from "@lekoarts/gatsby-theme-minimal-blog/src/components/navigation"
-import HeaderTitle from "@lekoarts/gatsby-theme-minimal-blog/src/components/header-title"
+import HeaderTitle from "./header-title"  // Use local shadowed component
 import HeaderExternalLinks from "@lekoarts/gatsby-theme-minimal-blog/src/components/header-external-links"
 
 const Header = () => {
