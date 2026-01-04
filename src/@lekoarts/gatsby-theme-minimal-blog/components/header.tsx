5830c5673cbf7e02f092a3b5c0bce8dbb1df055e
Jin Kim <jinyoungkim927@gmail.com>
2026-01-04 00:11:31 -0500

    Remove dark mode toggle - light mode only

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx, Flex } from "theme-ui"
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
+import Navigation from "@lekoarts/gatsby-theme-minimal-blog/src/components/navigation"
+import HeaderTitle from "@lekoarts/gatsby-theme-minimal-blog/src/components/header-title"
+import HeaderExternalLinks from "@lekoarts/gatsby-theme-minimal-blog/src/components/header-external-links"
+
+const Header = () => {
+  const { navigation: nav } = useMinimalBlogConfig()
+
+  return (
+    <header sx={{ mb: [5, 6] }}>
+      <Flex sx={{ alignItems: `center`, justifyContent: `space-between` }}>
+        <HeaderTitle />
+        {/* Color mode toggle removed - light mode only */}
+      </Flex>
+      <div
+        sx={{
+          boxSizing: `border-box`,
+          display: `flex`,
+          variant: `dividers.bottom`,
+          alignItems: `center`,
+          justifyContent: `space-between`,
+          mt: 3,
+          color: `secondary`,
+          a: { color: `secondary`, ":hover": { color: `heading` } },
+          flexFlow: `wrap`,
+        }}
+      >
+        <Navigation nav={nav} />
+        <HeaderExternalLinks />
+      </div>
+    </header>
+  )
+}
+
+export default Header
+
